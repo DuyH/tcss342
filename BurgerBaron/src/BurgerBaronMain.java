d342d8e810b9454293aa913282333fc7651a33d0
duyh <dhuynh88@uw.edu>
2015-04-03 16:18:05 -0700

    Added method prototypes to stack class, added main class.

---
+++ BurgerBaron/src/BurgerBaronMain.java
@@ -0,0 +1,19 @@
+public class BurgerBaronMain {
+
+	public static void main(String[] args) {
+
+	}
+
+	private static void parseLine(String line) {
+
+	}
+
+	private static void testMyStack() {
+
+	}
+
+	private static void testBurger() {
+
+	}
+
+}
