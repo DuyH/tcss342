05fccd43032cdd0745e6139f619da5392ffb6bf8
duyh <dhuynh88@uw.edu>
2015-05-22 21:08:48 -0700

    Changes to Main

--- CompressedLiterature/src/Main.java
+++ CompressedLiterature/src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-    }
+	}
+
+	private String convertTextToString() {
+		return null;
+	}
 
 }
