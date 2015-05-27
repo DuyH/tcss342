e007a391f2417e85c993404f9e443e5880d0be92
Duy Huynh <dhuynh88@uw.edu>
2015-05-27 10:55:29 -0700

    Added tests to Main, modified peskyLines test file.

--- CompressedLiterature/src/Main.java
+++ CompressedLiterature/src/Main.java
@@ -17,9 +17,9 @@ import java.nio.file.Paths;
 import java.util.Map;
 
 /**
- * Main program that takes in a text file and performs Huffman coding
- * algorithm, compresses the text file, and outputs the compressed
- * text file along with the Huffman codes in a seperate file.
+ * Main program that takes in a text file and performs Huffman coding algorithm,
+ * compresses the text file, and outputs the compressed text file along with the
+ * Huffman codes in a seperate file.
  * 
  * @author Duy Huynh
  * @version 20 May 2015
@@ -27,11 +27,15 @@ import java.util.Map;
  */
 public class Main {
 
-    // To test other text files, remove comments from one line and comment the others:
+    // To test other text files, remove comments from one line and comment the
+    // others:
     /** Name of text file to compress. */
     private static final String TEXT_FILE = "src/WarAndPeace.txt";
-    // private static final String TEXT_FILE = "src/The Adventures of Tom Sawyer.txt";
-    // private static final String TEXT_FILE = "src/peskyNewLines.txt";
+    // private static final String TEXT_FILE = "src/TheAdventuresofTomSawyer.txt";
+    // private static final String TEXT_FILE = "src/TheYellowWallpaper.txt";
+
+    /** Name of test file to compress. */
+    private static final String TEST_FILE = "src/peskyNewLines.txt";
 
     /** Name of file containing Huffman codes. */
     private static final String CODES_FILE = "src/codes.txt";
@@ -49,19 +53,23 @@ public class Main {
     private static final int BYTE = 8;
 
     /**
-     * Main program that takes a text file, uses Huffman coding to output compressed file.
+     * Main program that takes a text file, uses Huffman coding to output
+     * compressed file.
      * 
      * @param args Command line arguments
      */
     public static void main(final String[] args) {
+
+        // Testing
+        // testProgram();
 
         // Start timer:
         final long startTime = System.currentTimeMillis();
 
         // Convert a piece of text and insert into CodingTree
         // To test other files, remove comments in "Constants" section above
-        final CodingTree codingTree = new CodingTree(convertTextToString(TEXT_FILE,
-                Charset.defaultCharset()));
+        final CodingTree codingTree = new CodingTree(convertTextToString(
+                TEXT_FILE, Charset.defaultCharset()));
 
         // Create codes file
         createCodesFile(codingTree.codes);
@@ -70,33 +78,31 @@ public class Main {
         createBinaryFile(codingTree.bits);
 
         // Decode compressed message (uncomment to invoke):
-        // createDecodedFile(codingTree);
+        createDecodedFile(codingTree);
 
         // End timer:
         final long endTime = System.currentTimeMillis();
 
         // Display statistics:
-        final long uncompressedSize = new File(TEXT_FILE).length();
-        final long compressedSize = new File(BINARY_FILE).length();
-        System.out.println((endTime - startTime) + "ms");
-        System.out.println("Uncompressed: " + uncompressedSize + " bytes");
-        System.out.println("Compressed: " + compressedSize + " bytes");
-        System.out.printf("Compression ratio: %.2f%%\n", (double) compressedSize
-                / uncompressedSize * PERCENT);
+        displayStats(startTime, endTime);
 
     }
 
     /**
-     * Takes a text file and converts it into a readable String for manipulation.
-     * Adapted from http://stackoverflow.com/a/326440/3354122
+     * Takes a text file and converts it into a readable String for
+     * manipulation. Adapted from http://stackoverflow.com/a/326440/3354122
      * 
-     * @param textFile Original text file to be transferred to String.
-     * @param charSet Character encoding set.
+     * @param textFile
+     *            Original text file to be transferred to String.
+     * @param charSet
+     *            Character encoding set.
      * @return The text file converted into a String.
      */
-    private static String convertTextToString(final String textFile, Charset charSet) {
+    private static String convertTextToString(final String textFile,
+            Charset charSet) {
 
-        // Note**: Don't use Scanner because it eats away at the carriage returns...
+        // Note**: Don't use Scanner because it eats away at the carriage
+        // returns...
         byte[] encoded = null;
         try {
             encoded = Files.readAllBytes(Paths.get(textFile));
@@ -107,9 +113,11 @@ public class Main {
     }
 
     /**
-     * Creates a file containing characters from a text and their respective Huffman code.
+     * Creates a file containing characters from a text and their respective
+     * Huffman code.
      * 
-     * @param codes Map of Characters and their Huffman code.
+     * @param codes
+     *            Map of Characters and their Huffman code.
      */
     private static void createCodesFile(final Map<Character, String> codes) {
 
@@ -125,12 +133,14 @@ public class Main {
     /**
      * Writes binary file output of the encoded String.
      * 
-     * @param encodedString The encoded String in 'binary' 1's and 0's for file output.
+     * @param encodedString
+     *            The encoded String in 'binary' 1's and 0's for file output.
      */
     private static void createBinaryFile(final String encodedString) {
         try {
 
-            // Note to future self: Don't use FileOutputStream; it's SLOW!!! (4seconds)
+            // Note to future self: Don't use FileOutputStream; it's SLOW!!!
+            // (4seconds)
             final ObjectOutputStream output = new ObjectOutputStream(
                     new FileOutputStream(BINARY_FILE));
 
@@ -142,7 +152,8 @@ public class Main {
 
             // Write to binary file, 8 'bits' at a time.
             for (int i = 0; i < encodedString.length(); i += BYTE) {
-                output.write(Integer.parseInt(fixedString.substring(i, i + BYTE), 2));
+                output.write(Integer.parseInt(
+                        fixedString.substring(i, i + BYTE), 2));
             }
             output.close();
 
@@ -154,10 +165,12 @@ public class Main {
     /**
      * Decodes a compress String to output file.
      * 
-     * @param codingTree The CodingTree used to compress original file to be decoded.
+     * @param codingTree
+     *            The CodingTree used to compress original file to be decoded.
      */
     private static void createDecodedFile(final CodingTree codingTree) {
-        final String decodedString = codingTree.decode(codingTree.bits, codingTree.codes);
+        final String decodedString = codingTree.decode(codingTree.bits,
+                codingTree.codes);
         try {
             final FileWriter writer = new FileWriter(DECODED_FILE);
             writer.write(decodedString);
@@ -165,6 +178,56 @@ public class Main {
         } catch (final IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Display compression statistics.
+     * 
+     * @param startTime
+     *            Time when compression started.
+     * @param endTime
+     *            Time when compression ended.
+     */
+    private static void displayStats(final long startTime, final long endTime) {
+
+        // http://en.wikipedia.org/wiki/Data_compression_ratio#Definitions
+        final long uncompressedSize = new File(TEXT_FILE).length() * BYTE;
+        final long compressedSize = new File(BINARY_FILE).length() * BYTE;
+        System.out.println("Converted " + TEXT_FILE.substring(4));
+        System.out.println("Elapsed time: " + (endTime - startTime) + "ms");
+        System.out.println("Uncompressed: " + uncompressedSize + " bits");
+        System.out.println("Compressed: " + compressedSize + " bits");
+        System.out.printf("Compression ratio: %.2f%%\n",
+                (double) uncompressedSize / compressedSize * PERCENT);
+    }
+
+    /** Tests various components of program. */
+    private static void testProgram() {
+
+        // Test reading in a text file (first 1000 characters)
+        String test = convertTextToString(TEST_FILE, Charset.defaultCharset());
+        System.out.println("Original uncompressed text:");
+        System.out.println(test);
+        System.out.println("=========================");
+
+        // Create a coding tree from the text string
+        final CodingTree testTree = new CodingTree(test);
+
+        // Print out the codes
+        System.out.println("Huffman codes:");
+        System.out.println(testTree.codes);
+        System.out.println("=========================");
+
+        // Print out the compressed bits string (just the first 100)
+        System.out.println("Compressed bits:");
+        System.out.println(testTree.bits);
+        System.out.println("=========================");
+
+        // Test decode by decoding first 100 bits
+        System.out.println("Compressed bits decoded:");
+        String decodedString = testTree.decode(testTree.bits, testTree.codes);
+        System.out.println(decodedString);
+
     }
 
 }
