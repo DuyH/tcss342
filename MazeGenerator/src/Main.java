f126d9d3222ab90b7470fbe021989b0c6a952588
Duy Huynh <dhuynh88@uw.edu>
2015-06-11 20:41:17 -0700

    Finished assignment. Want to work on GUI later.

--- MazeGenerator/src/Main.java
+++ MazeGenerator/src/Main.java
@@ -1,20 +1,38 @@
+/*
+ * Duy Huynh
+ * TCSS 342 - Spring '15
+ * Assignment 5 - Maze Generator
+ * Main.java
+ */
+
+/**
+ * Main that creates mazes of various sizes.
+ * 
+ * @author Duy Huynh
+ * @version 01 June 2015
+ *
+ */
 public class Main {
 
+    /**
+     * Main.
+     * 
+     * @param args Command line arguments.
+     */
     public static void main(String[] args) {
 
-        
         // Create a 5x5 maze with debugging
-        Maze maze = new Maze(2,3,true);
+        Maze maze = new Maze(5, 5, true);
         maze.display();
-        System.out.println();
-        
-        // Create a larger maze with debugging off:
-        maze = new Maze(10,10,false);
+
+        // Create 5x5 maze without debugging
+        maze = new Maze(5, 5, false);
         maze.display();
-        System.out.println();
-        
-        // Testing and debugging:
-        
+
+        // Create 10x10 maze without debugging
+        maze = new Maze(10, 10, false);
+        maze.display();
+
     }
 
 }
--- MazeGenerator/src/Maze.java
+++ MazeGenerator/src/Maze.java
@@ -1,146 +1,251 @@
+/*
+ * Duy Huynh
+ * TCSS 342 - Spring '15
+ * Assignment 5 - Maze Generator
+ * Maze.java
+ */
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
+/**
+ * Generates n x m maze with solution.
+ * 
+ * @author Duy Huynh
+ * @version 01 June 2015
+ *
+ */
 public class Maze {
 
-	// Walls
-	private static final int NORTH = 8; // 1000
-	private static final int EAST = 2; // 0010
-	private static final int SOUTH = 1; // 0001
-	private static final int WEST = 4; // 0100
+    /** Width of maze. */
+    private int width;
 
-	private int width;
-	private int depth;
-	private boolean debug;
-	private int verticies;
-	private int verticiesVisited;
+    /** Depth of maze. */
+    private int depth;
 
-	private Cell cells[][];
+    /** Whether to show maze debugging. */
+    private boolean debug;
 
-	/*
-	 * W x H maze r = row, c = columns v = vertex = r*W+c Given v: r = v/W c =
-	 * v%W
-	 */
+    /** Random generator for choosing adjacent cells. */
+    private Random random;
 
-	public Maze(int width, int depth, boolean debug) {
+    /** Matrix containing vertices (cells) of maze. */
+    private Cell cells[][];
 
-		// Initialize variables:
-		this.width = width;
-		this.depth = depth;
-		this.debug = debug;
-		verticies = width * depth;
-		verticiesVisited = 0;
+    /** Matrix containing debug display data. */
+    private char[][] debugDisplay;
 
-		generateGrid(); // create grid of cells
+    /**
+     * Construct n x m maze with solution.
+     * 
+     * @param width Width of maze.
+     * @param depth Depth of maze.
+     * @param debug Whether or not maze debugging is shown.
+     */
+    public Maze(int width, int depth, boolean debug) {
 
-		createAdjencyLists(); // Create adjacency list for each cell
+        // Initialize variables:
+        random = new Random();
+        this.width = width;
+        this.depth = depth;
+        this.debug = debug;
 
-	}
+        // Setup display grid
+        generateDisplayGrid();
 
-	private void generateGrid() {
-		// 2D array; each element is a Cell
-		cells = new Cell[depth][width];
-		for (int r = 0; r < depth; r++) {
-			for (int c = 0; c < width; c++) {
-				System.out.println("row: " + r + " col: " + c);
-				cells[r][c] = new Cell(r, c);
-			}
-		}
-	}
+        // Create grid of cells
+        generateGrid();
 
-	private void createAdjencyLists() {
+        // Create adjacency list for each cell
+        createAdjacencyLists();
 
-		for (int r = 0; r < depth; r++) {
-			for (int c = 0; c < width; c++) {
-				System.out.println("row: " + r + " col: " + c);
-				ArrayList<Cell> temp = new ArrayList<Cell>();
-				// If cell is in first column, no adjacent left cell:
-				if (c != 0)
-					temp.add(cells[r][c - 1]);
-				// If cell is in last column, no adjacent right cell:
-				if (c != width - 1)
-					temp.add(cells[r][c + 1]);
-				// If cell is in top row, no adjacent cell above:
-				if (r != 0)
-					temp.add(cells[r - 1][c]);
-				// If cell is in bottom row, no adjacent cell below:
-				if (r != depth - 1)
-					temp.add(cells[r + 1][c]);
-				// Set this cell's adjacency list:
-				cells[r][c].adjacentCells = temp;
-			}
-		}
-	}
+        // Seed the recursive depth-first-search with top-left cell
+        explore(cells[0][0]);
 
-	private void explore(Cell parent, Cell adjacent, int wall) {
+        // If debugging was turned on, now turn it off:
+        this.debug = false;
 
-		if (verticiesVisited < verticies) {
-			if (parent != null) {
-				// Knockdown parent's wall
-				parent.knockDownWall(wall);
-				// Knockdown child's wall
-				adjacent.knockDownWall(Integer.reverse(wall) >> 28);
-				// as long as arraylist has elements, select one of them, else return
-			}
-		}
+    }
 
-	}
+    /** Generate the matrix that holds display data for the console. */
+    private void generateDisplayGrid() {
 
-	public void display() {
+        // 2D char array for displaying maze in console:
+        int displayDepth = depth * 2 + 1;
+        int displayWidth = width * 2 + 1;
+        debugDisplay = new char[depth * 2 + 1][width * 2 + 1];
+        for (int r = 0; r < displayDepth; r++) {
+            for (int c = 0; c < displayWidth; c++) {
+                debugDisplay[r][c] = 'X';
+            }
+        }
+        debugDisplay[0][1] = ' '; // Create entrance
+        debugDisplay[displayDepth - 1][displayWidth - 2] = ' '; // Create exit
+    }
 
-	}
+    /** Generate the matrix that holds a vertex in each cell. */
+    private void generateGrid() {
 
-	/**
-	 * Cells represent a vertex in the graph.
-	 * 
-	 * @author Duy Huynh
-	 * @version 06/03/2015
-	 *
-	 */
-	public class Cell {
+        // 2D array; each element is a vertex of the maze
+        cells = new Cell[depth][width];
+        for (int r = 0; r < depth; r++) {
+            for (int c = 0; c < width; c++) {
+                cells[r][c] = new Cell(r, c);
+                debugDisplay[r * 2 + 1][c * 2 + 1] = ' ';
+            }
+        }
+    }
 
-		private int walls;
-		private Cell parent; // Useful for backtracking
-		private int vertexNum; // Calculated: v = vertex = r*W+c
-		private List<Cell> adjacentCells; // Neighboring cells to this one
-		private boolean visited; // Record if cell has been visited during
-									// pathfinding
-		private int row;
-		private int col;
+    /** Create a list of adjacent vertices for each vertex. */
+    private void createAdjacencyLists() {
 
-		public Cell(int row, int col) {
-			walls = 0xf; // 1111 (walls intact)
-			this.parent = null;
-			this.visited = false;
-			this.adjacentCells = null;
-			this.row = row;
-			this.col = col;
-		}
+        // For each cell in the matrix, create an adjacency list.
+        for (int r = 0; r < depth; r++) {
+            for (int c = 0; c < width; c++) {
+                ArrayList<Cell> temp = new ArrayList<Cell>();
+                // If cell is in first column, no adjacent left cell:
+                if (c != 0)
+                    temp.add(cells[r][c - 1]);
+                // If cell is in last column, no adjacent right cell:
+                if (c != width - 1)
+                    temp.add(cells[r][c + 1]);
+                // If cell is in top row, no adjacent cell above:
+                if (r != 0)
+                    temp.add(cells[r - 1][c]);
+                // If cell is in bottom row, no adjacent cell below:
+                if (r != depth - 1)
+                    temp.add(cells[r + 1][c]);
+                // Set this cell's adjacency list:
+                cells[r][c].adjacentCells = temp;
+            }
+        }
+    }
 
-		public int getWalls() {
-			return walls;
-		}
+    /**
+     * Explore adjacent, unvisited cells, recursively until all have been explored.
+     * 
+     * @param curCell The current vertex/cell in the maze.
+     */
+    private void explore(Cell curCell) {
 
-		public void knockDownWall(int wall) {
-			this.walls &= ~wall; // Knock down a wall in binary
-		}
+        // Mark this cell as visited
+        curCell.visited = true;
+        debugDisplay[curCell.row * 2 + 1][curCell.col * 2 + 1] = 'V';
 
-		public int getRow() {
-			return vertexNum / width;
-		}
+        // Display debugging in console:
+        if (debug) display();
 
-		public int getCol() {
-			return vertexNum % width;
-		}
+        // Retrieve list of unvisited adjacent cells for the current cell
+        ArrayList<Cell> unvisitedCells = new ArrayList<Cell>();
+        for (Cell c : cells[curCell.row][curCell.col].adjacentCells) {
+            if (!c.visited) unvisitedCells.add(c);
+        }
 
-		public boolean getVisited() {
-			return visited;
-		}
+        // If unvisited adjacent cells exist, randomly choose one and explore it
+        // Also knock-down the walls between current cell and the chosen cell to explore
+        if (unvisitedCells.isEmpty()) { // no more unvisited adjacent cells
+            if (curCell.parent != null) explore(curCell.parent);// Backtrack
+        } else {
+            // Randomly choose an unvisited cell to explore:
+            int cellToExplore = random.nextInt(unvisitedCells.size());
+            Cell temp = unvisitedCells.get(cellToExplore);
+            temp.parent = curCell; // set parent cell so we can backtrack later
 
-		public void setVisited(boolean visited) {
-			this.visited = visited;
-		}
+            // Knock-down walls before exploring next cell:
+            if (curCell.row < temp.row) {
+                curCell.south = temp.north = false;
+                debugDisplay[curCell.row * 2 + 2][curCell.col * 2 + 1] = ' ';
+            } else if (curCell.row > temp.row) {
+                curCell.north = temp.south = false;
+                debugDisplay[curCell.row * 2][curCell.col * 2 + 1] = ' ';
+            } else if (curCell.col < temp.col) {
+                curCell.east = temp.west = false;
+                debugDisplay[curCell.row * 2 + 1][curCell.col * 2 + 2] = ' ';
+            } else if (curCell.col > temp.col) {
+                curCell.west = temp.east = false;
+                debugDisplay[curCell.row * 2 + 1][curCell.col * 2] = ' ';
+            }
 
-	}
+            // After knocking down walls, go ahead and move to next cell
+            explore(temp);
+        }
+    }
 
+    /** Clear debugging matrix of path-finding or solution indicators. */
+    private void clearIndicators() {
+
+        // Clear all path-finding characters from debug matrix:
+        for (int r = 0; r < depth; r++) {
+            for (int c = 0; c < width; c++) {
+                debugDisplay[r * 2 + 1][c * 2 + 1] = ' ';
+            }
+        }
+    }
+
+    /** Indicate solution path in debugging matrix. */
+    private void loadSolution() {
+
+        // Start at the end and backtrack through the parents, marking the solution path
+        Cell solution = cells[depth - 1][width - 1];
+        while (solution != null) {
+            debugDisplay[solution.row * 2 + 1][solution.col * 2 + 1] = '+';
+            solution = solution.parent;
+        }
+
+    }
+
+    /** Takes the debug data from debugging matrix. */
+    public void display() {
+
+        // Clear path-finding indicators before displaying maze
+        if(!debug){
+            clearIndicators();
+            loadSolution();
+        }
+        
+        // Go through each cell of the debugging matrix and print data to console
+        for (int r = 0; r < depth * 2 + 1; r++) {
+            for (int j = 0; j < width * 2 + 1; j++) {
+                System.out.print(debugDisplay[r][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    /**
+     * Cells represent a vertex in the graph.
+     * 
+     * @author Duy Huynh
+     * @version 06/03/2015
+     *
+     */
+    private class Cell {
+
+        /** This cell's parent in traversing the maze.*/
+        private Cell parent; // Useful for backtracking
+        
+        /** List of this cell's adjacent vertices.*/
+        private List<Cell> adjacentCells; // Neighboring cells to this one
+        
+        /** Row and column indices.*/
+        private int row, col;
+        
+        /** Whether the walls are open or not.*/
+        private boolean visited, north, south, east, west;
+
+        /** Create a cell (vertex) belonging to the maze.
+         * @param row Row index of cell.
+         * @param col Column index of cell.
+         */
+        private Cell(int row, int col) {
+            this.parent = null;
+            this.adjacentCells = null;
+            this.row = row;
+            this.col = col;
+            visited = false;
+            north = south = east = west = true;
+        }
+    }
 }
